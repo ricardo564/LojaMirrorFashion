b77417f55eec0fb5228e9092befee8cf067ffcab
Ricardo564 <ricardo_hks@hotmail.com>
2019-06-19 19:25:33 -0300

    preparando para receber paginas traduzidas

--- english/conexao_bancophp.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conexao = mysqli_connect("127.0.0.1","root","","wd43");
-	$dados = mysqli_query($conexao,"SELECT * FROM produtos");
-	
-	$dadosF = mysqli_fetch_all($dados);
-	$produtos = mysqli_fetch_array($dados);
-?>
--- register.php
+++ register.php
@@ -12,13 +12,16 @@ include("conexao_bancophp.php");
 	<div class="container text-center col-md-4 col-md-offset-3 mb-5 div_form_loggin_register" ><!--2-->		
 		<div id="mirror_form_register" >
 			<form  method="POST" action="register.php"><!--formulario para cadastro-->
+				<label  for="mirror_name_register">nome</label>
+				<input class="mb-1 form-control" id="mirror_name_register" name="mirror_name_register" type="text" placeholder="Meng Hao">
+
 				<label  for="mirror_email_register">Email</label>
-				<input class="mb-1 form-control" id="mirror_email_register" type="text" placeholder="TrueImmortalEmail@NovesReinos.cultivo">
+				<input class="mb-1 form-control" id="mirror_email_register" name="mirror_email_register" type="text" placeholder="TrueImmortalEmail@NovesReinos.cultivo">
 
 				<br>
 
 				<label  for="mirror_password_register">Senha</label>
-				<input class="form-control" id="mirror_password_register" type="password" placeholder="Texto Misterioso">
+				<input class="form-control" id="mirror_password_register" name="mirror_password_register" type="password" placeholder="123456">
 
 				<br>
 
@@ -32,6 +35,17 @@ include("conexao_bancophp.php");
 	</div><!--janela principal inicio-->
 
 	<?php
+	$user_register_name = @$_POST['mirror_name_register'];
+	$user_register_mail = @$_POST['mirror_email_register'];
+	$user_register_password = @$_POST['mirror_password_register'];
+
+	if(!empty($user_register_name)||!empty($user_register_mail)||!empty($user_register_password)){
+		$sql_register="INSERT INTO users(user_name,user_mail,user_password) VALUES ('$user_register_name','$user_register_mail','$user_register_password')";
+
+		$rs = mysqli_query($conexao, $sql_register);
+		mysqli_close($conexao);
+	}
+
 
 	?>
 

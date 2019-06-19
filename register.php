6cfd6afb65a2fba318210b3139adcccea8b9b552
Ricardo564 <ricardo_hks@hotmail.com>
2019-06-19 15:48:43 -0300

    register.php e login.php criados

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 $cabecalho_title='Login/Cadastro';
-include("cabecalho_bootstrap.php")
+include("cabecalho_bootstrap.php");
+include("conexao_bancophp.php");
 ?>
 <head>
 	<link rel="stylesheet" href="css/keyframes.css">
@@ -10,7 +11,7 @@ include("cabecalho_bootstrap.php")
 
 	<div class="container text-center col-md-4 col-md-offset-3 mb-5 div_form_loggin_register" ><!--2-->		
 		<div id="mirror_form_register" >
-			<form  method="POST" action="login_register.php"><!--formulario para cadastro-->
+			<form  method="POST" action="register.php"><!--formulario para cadastro-->
 				<label  for="mirror_email_register">Email</label>
 				<input class="mb-1 form-control" id="mirror_email_register" type="text" placeholder="TrueImmortalEmail@NovesReinos.cultivo">
 
@@ -22,15 +23,17 @@ include("cabecalho_bootstrap.php")
 				<br>
 
 				<div class=" d-flex align-items-center justify-content-center">
-					<button class="btn btn-primary btn-lg">CADASTRAR<0/button>		
+					<button class="btn btn-primary btn-lg" name="register">CADASTRAR</button>		
 				</div>						
 			</form><!--/formulario para cadastro-->
 		</div>
-		
+
 
 	</div><!--janela principal inicio-->
 
+	<?php
 
+	?>
 
 
 	<?php

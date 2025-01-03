950d76f178ebb9ab8b407ad6b38e21abcb9472ff
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 11:20:40 -0300

    Refactor HTML and CSS files for improved formatting and consistency; update JavaScript for better functionality. Enhanced navbar structure, applied consistent styling across CSS files, and optimized JavaScript event handling for better performance.

--- js/baner.js
+++ js/baner.js
@@ -1,26 +1,23 @@
-window.onload = function(){
-	var banners= ["img/destaque-home.png", "img/destaque-home-2.png"]
-	var bannerAtual = 0;
+window.onload = function () {
+  var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+  var bannerAtual = 0;
 
-	function trocaBanner(){
-		bannerAtual= (bannerAtual + 1) % 2;
-		document.querySelector('.banner-destaque img').src = banners[bannerAtual];
-	}
+  function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector(".banner-destaque img").src = banners[bannerAtual];
+  }
 
-	var timer = setInterval(trocaBanner, 3000);
-	var controle = document.querySelector('.pause');
+  var timer = setInterval(trocaBanner, 3000);
+  var controle = document.querySelector(".pause");
 
-	controle.onclick = function(){
-		if (controle.className == 'pause'){
-			clearInterval(timer);
-			controle.className = 'play';
-		} else {
-			timer= setInterval(trocaBanner, 3000);
-			controle.className= 'pause';
-		}
-		return false;
-	}
-
-
-
-}
+  controle.onclick = function () {
+    if (controle.className == "pause") {
+      clearInterval(timer);
+      controle.className = "play";
+    } else {
+      timer = setInterval(trocaBanner, 3000);
+      controle.className = "pause";
+    }
+    return false;
+  };
+};
--- js/total.js
+++ js/total.js
@@ -1,24 +1,18 @@
+window.onload = function () {
+  /*função para calcular o total*/
+  var $input_quantidade = document.querySelector("#clothing_quantity");
+  var $output_total = document.querySelector("#clothing_total_price");
 
-window.onload = function(){
+  $input_quantidade.oninput = function () {
+    var preco = document.querySelector("#clothing_price").textContent;
+    preco = preco.replace("R$ ", "");
+    preco = preco.replace(",", ".");
+    preco = parseFloat(preco);
 
-	/*função para calcular o total*/
-	var	$input_quantidade	=	document.querySelector("#clothing_quantity");
-	var	$output_total	=	document.querySelector("#clothing_total_price");
-
-	$input_quantidade.oninput	=	function() {
-
-	var	preco	=	document.querySelector("#clothing_price").textContent;
-		preco	=	preco.replace("R$ ",	"");
-		preco	=	preco.replace(",",	".");
-		preco	=	parseFloat(preco);
-
-	var	quantidade	=	$input_quantidade.value;
-	var	total	=	quantidade	*	preco;
-		total	=	"R$	"	+	total.toFixed(2)
-		total	=	total.replace(".",	",");
-		$output_total.value	=	total;
-}/*/função para calcular o total*/
-
-
-
-}
+    var quantidade = $input_quantidade.value;
+    var total = quantidade * preco;
+    total = "R$	" + total.toFixed(2);
+    total = total.replace(".", ",");
+    $output_total.value = total;
+  }; /*/função para calcular o total*/
+};
